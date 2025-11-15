b90a26ad39f131bb722d67ac98d42445f6e71061
GitHub Action <action@github.com>
2025-11-15 10:14:32 +0000

    Commit pending changes

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -11,9 +11,9 @@
  * Current application version - Automatically updated from package.json
  * @see package.json version field
  * @see config/vite-plugin-version-sync.ts for sync implementation
- * CURRENT PACKAGE.JSON VERSION: 1.1.41
+ * CURRENT PACKAGE.JSON VERSION: 1.1.42
  */
-export const APP_VERSION = '1.1.41'
+export const APP_VERSION = '1.1.42'
 
 /**
  * Application name
